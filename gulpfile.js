d4f04acd8e2371896416bfe46cffda0bd24b9fa1
Rolf Wessels <rolf.wessels@gmail.com>
2019-08-22 10:25:58 +0200

    Adding another redirect.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,30 @@
-var gulp = require('gulp');
-var ts = require('gulp-typescript');
- 
-gulp.task('default', function () {
-    return gulp.src('src/**/*.ts')
-        .pipe(ts({
-            noImplicitAny: true,
-            outFile: 'redirect.js'
-        }))
-        .pipe(gulp.dest('dist'));
+var gulp = require("gulp");
+var ts = require("gulp-typescript");
+var connect = require("gulp-connect");
+
+gulp.task("build", function() {
+  return gulp
+    .src("src/**/*.ts")
+    .pipe(
+      ts({
+        noImplicitAny: true,
+        outFile: "redirect.js"
+      })
+    )
+    .pipe(gulp.dest("dist"))
+    .pipe(connect.reload());
 });
+
+gulp.task("watch", function() {
+  gulp.watch(["src/*.ts"], gulp.parallel("build"));
+});
+
+gulp.task("connect", function() {
+  connect.server({
+    root: "./dist",
+    livereload: true
+  });
+});
+
+gulp.task("default", gulp.parallel("build"));
+gulp.task("start", gulp.parallel("build", "connect", "watch"));
