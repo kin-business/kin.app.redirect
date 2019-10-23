3d8ae86f5e7754668f5a29eb0c9558e144f08711
Rolf Wessels <rolf.wessels@gmail.com>
2019-10-23 09:09:48 +0200

    Automatic redirect to web.kin.me for group and invite.

--- dist/redirect.js
+++ dist/redirect.js
@@ -1,108 +1,38 @@
-var LinkOptions = /** @class */ (function () {
-    function LinkOptions() {
-    }
-    return LinkOptions;
-}());
-function getMobileOperatingSystem() {
-    var userAgent = navigator.userAgent || navigator.vendor || window.window.opera;
-    if (/android/i.test(userAgent)) {
-        return "android";
-    }
-    // iOS detection from: http://stackoverflow.com/a/9039885/177710
-    if (/iPad|iPhone|iPod/.test(userAgent) && !window.window.MSStream) {
-        return "ios";
-    }
-    return "web";
-}
-function prepareLinks(linkCheck, slug, newUrl) {
-    var links = document.getElementsByTagName("a");
-    for (var i = 0; i < links.length; i++) {
-        var link = links[i];
-        var source = link.getAttribute("rel");
-        var queryParams = window.location.search.indexOf("?") === 0
-            ? window.location.search.substr(1, window.location.search.length - 1)
-            : window.location.search;
-        var search = location.search.substring(1);
-        var d = JSON.parse('{"' + search.replace(/&/g, '","').replace(/=/g, '":"') + '"}', function (key, value) {
-            return key === "" ? value : decodeURIComponent(value);
-        });
-        console.log("d", d);
-        if (source != null && source.indexOf(linkCheck) >= 0) {
-            link.href = newUrl
-                .replace("{qp}", queryParams)
-                .replace("{slug}", slug)
-                .replace("{qpenc}", encodeURIComponent(queryParams))
-                .replace("{uid}", d.uid);
-            return true;
-        }
-    }
-    return false;
-}
+var LinkOptions = /** @class */ (function() {
+  function LinkOptions() {}
+  return LinkOptions;
+})();
+
 function readSlug() {
-    return window.location.pathname.replace(/\//g, "") || "home";
+  return window.location.pathname.replace(/\//g, "") || "home";
 }
+
 function injectLinks() {
-    var channel = getMobileOperatingSystem();
-    var slug = readSlug();
-    var links = buildLinks();
-    // console.log("channel", channel);
-    // console.log("slug", slug);
-    // console.log("links", links);
-    if (links[channel]) {
-        if (links[channel].redirect !== null) {
-            console.log("Redirect:" + links[channel].redirect);
-        }
-        prepareLinks("download", slug, links[channel].store);
-        prepareLinks("open-app", slug, links[channel].launch);
-    }
-}
-function getParameterByName(name, url) {
-    if (!url)
-        url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"), results = regex.exec(url);
-    if (!results)
-        return null;
-    if (!results[2])
-        return "";
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
-function buildLinks() {
-    var links = {};
+  var slug = readSlug();
+  if (slug == "group" || slug == "invite") {
     var env = getParameterByName("env", window.location.href);
+    var uid = getParameterByName("uid", window.location.href);
+    var link =
+      slug == "group"
+        ? `https://web.kin.me/home/kins/group/${uid}`
+        : `https://web.kin.me/invite/${uid}`;
     if (env == "dev") {
-        links["android"] = {
-            store: "https://play.google.com/store/apps/details?id=com.thekinapp.dev&pcampaignid=fdl_long&url=https://thekinapp.com/{slug}?{qpenc}",
-            launch: "https://thekinapp.com/{slug}?{qp}",
-            redirect: null
-        };
-        links["ios"] = {
-            store: "https://itunes.apple.com/app/id1437611153",
-            launch: "com.thekinapp.dev://{slug}?{qp}",
-            redirect: null
-        };
-        links["web"] = {
-            store: "https://web.dev.thekinapp.com/home/kins/{slug}/{uid}?&redirect={qpenc}",
-            launch: "https://web.dev.thekinapp.com/home/kins/{slug}/{uid}?&redirect={qpenc}",
-            redirect: "/clickfrommobile"
-        };
+      var link =
+        slug == "group"
+          ? `https://web.dev.kin.me/home/kins/group/${uid}`
+          : `https://web.dev.kin.me/invite/${uid}`;
     }
-    else {
-        links["android"] = {
-            store: "https://play.google.com/store/apps/details?id=com.thekinapp.prod&pcampaignid=fdl_long&url=https://thekinapp.com/{slug}?{qpenc}",
-            launch: "https://thekinapp.com/{slug}?{qp}",
-            redirect: null
-        };
-        links["ios"] = {
-            store: "https://itunes.apple.com/app/id1437611153",
-            launch: "com.thekinapp://{slug}?{qp}",
-            redirect: null
-        };
-        links["web"] = {
-            store: "https://web.thekinapp.com/home/kins/{slug}/{uid}?&redirect={qpenc}",
-            launch: "https://web.thekinapp.com/home/kins/{slug}/{uid}?&redirect={qpenc}",
-            redirect: "/clickfrommobile"
-        };
-    }
-    return links;
+    console.log("Redirect to ", link);
+    document.location = link;
+  }
+}
+
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return "";
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
